1526a9839e243c7b44df1c7409898a206eb7e94b
ankit <ankitpawar193@gmail.com>
2025-04-12 16:01:00 +0530

    12 apr commit 1a ankit

---
+++ src/main/java/pages/GUIElements.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class GUIElements extends BaseTest {
+
+	@Test
+	public void m() {
+		System.out.println("akshu is best Autoamtion QA");
+	}
+
+}
