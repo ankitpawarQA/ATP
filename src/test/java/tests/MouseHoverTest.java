6c5211b5bdf6c40d150485d5c6493201c24c5dce
ankit <ankitpawar193@gmail.com>
2025-04-16 09:09:47 +0530

    16 apr 2c

---
+++ src/test/java/tests/MouseHoverTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class MouseHoverTest extends BaseTest {
+
+	@Test
+	public void validateMouseHover() {
+
+	}
+
+}
