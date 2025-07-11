b1208fca0842117eecb246596e655ce3968a8adc
ankit <ankitpawar193@gmail.com>
2025-07-11 10:19:15 +0530

    changes for jenkins push

--- src/test/java/tests/GUIElementsTest.java
+++ src/test/java/tests/GUIElementsTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
-import junit.framework.Assert;
+
 import pages.GUIElements;
 
 public class GUIElementsTest extends BaseTest {
